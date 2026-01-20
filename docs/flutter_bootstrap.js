7b55329c43a7b5faecddae538aee056cdc397af2
srivathsan1510 <srivathsan1510@gmail.com>
2026-01-20 22:38:36 +0530

    Fix white screen path

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "573594802"
+    serviceWorkerVersion: "2065183613"
   }
 });
